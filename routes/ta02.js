c5529f5dda60240b0bbbede387ddcd92c61cc02f
Dana <djsilvertail@byui.edu>
2021-09-22 10:26:09 -0700

    pushing up team assignment week two

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,9 +3,31 @@
 const express = require('express');
 const router = express.Router();
 
+//For requirement 01
+const userArray = ['Dana', 'Mel', 'Mickal'];
+
+//For requirement 02
+router.post('/addUser', (req, res, next) => {
+  const newUser = req.body.newUser;
+  userArray.push(newUser);
+  res.redirect('/ta02/');
+});
+
+//For requirement 03
+router.post('/removeUser', (req, res, next) => {
+  const remUser = req.body.remUser;
+  const userArray
+  const index = userArray.indexOf(remUser);
+  if (index !== -1) {
+    userArray.splice(index, 1);
+  }
+  res.redirect('/ta02');
+});
+
 router.get('/', (req, res, next) => {
   res.render('pages', {
     title: 'Team Activity 02',
+    users: userArray,
     path: '/ta02', // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
