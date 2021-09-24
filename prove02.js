fc58a23803a92e346b26f69f2206348cce57604b
Dana <djsilvertail@byui.edu>
2021-09-24 12:31:43 -0700

    still working on prove02

--- prove02.js
+++ prove02.js
@@ -1,8 +1,13 @@
+const path = require('path');
+
 const express = require('express');
 
 const mainRoutes = require('./routes/prove02-routes.js');
+const { dirname } = require('path');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '..', 'public', 'scripts', 'main.js')))
 
 app.use(mainRoutes);
 
--- routes/prove02-routes.js
+++ routes/prove02-routes.js
@@ -1,13 +1,15 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('Route1');
+    res.sendFile(path.join(__dirname, '..', 'views', 'pages', 'prove02-index.html'));
 });
 
 router.get('/users', (req, res, next) => {
-    res.send('Route2');
+    res.sendFile(path.join(__dirname, '..', 'views', 'pages', 'prove02-users.html'));
 });
 
 module.exports = router;
